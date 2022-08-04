38f36413b910a284586ed9135be0c4c68797860f
Arthuthu <arthurgeromello@hotmail.com>
2022-08-04 17:12:48 -0300

    Created Database connection

---
+++ Business/Controller/GetAlunos.cs
@@ -0,0 +1,23 @@
+﻿using Business.Connection;
+using Business.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Controller
+{
+    public class GetAlunos
+    {
+        public List<Alunos> GetAlunosList()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("TopicosII")))
+            {
+                return connection.Query<Alunos>($"select * from Alunos").ToList();
+            }
+        }
+    }
+}
