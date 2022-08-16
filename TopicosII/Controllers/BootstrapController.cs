d8df382264fdefa57f40648be1a87e6610c5fab8
Arthuthu <arthurgeromello@hotmail.com>
2022-08-16 18:26:28 -0300

    Site changes

---
+++ TopicosII/Controllers/BootstrapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TopicosII.Controllers
+{
+    public class BootstrapController : Controller
+    {
+        // GET: Bootstrap
+
+        // O nome dessa action devera ser igual o nome da view (arquivo .cshtml)
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
