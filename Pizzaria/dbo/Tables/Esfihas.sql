a289064ede777033b765fbf5ed7058d8a946638f
Arthuthu <arthurgeromello@hotmail.com>
2022-08-21 20:26:12 -0300

    Added more html and bootstrap content in the index pages, added the Esfihas tambem for further development.

---
+++ Pizzaria/dbo/Tables/Esfihas.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Esfihas]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [IdTipo] INT NOT NULL, 
+    [Nome] NVARCHAR(50) NOT NULL, 
+    [Descricao] NVARCHAR(1000) NULL, 
+    [Quantidade] INT NOT NULL, 
+    [Valor] DECIMAL(18, 2) NOT NULL, 
+    [Ativo] BIT NOT NULL, 
+    CONSTRAINT [FK_Esfihas_ToTable] FOREIGN KEY (IdTipo) REFERENCES Tipos(Id),
+
+)
