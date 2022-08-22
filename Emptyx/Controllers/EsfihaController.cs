2f5161e76b43a45b942b09fa76a1978e94a2576a
Arthuthu <arthurgeromello@hotmail.com>
2022-08-21 22:47:35 -0300

    Edited logo in the nav bar, fixed esfiha view bug.

---
+++ Emptyx/Controllers/EsfihaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Emptyx.Controllers
+{
+    public class EsfihaController : Controller
+    {
+        // GET: Esfiha
+        public ActionResult Esfihas()
+        {
+            return View();
+        }
+    }
+}
