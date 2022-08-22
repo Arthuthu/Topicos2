8776002468cbfd437652b9903494c2b9345b5e52
Arthuthu <arthurgeromello@hotmail.com>
2022-08-21 23:18:15 -0300

    Added content to Esfihas page.

--- Emptyx/Controllers/EsfihaController.cs
+++ Emptyx/Controllers/EsfihaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PizzariaLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace Emptyx.Controllers
         // GET: Esfiha
         public ActionResult Esfihas()
         {
+            ViewBag.Esfihas = new GetEsfihas().Listar();
             return View();
         }
     }
--- PizzariaLibrary/Models/Esfihas.cs
+++ PizzariaLibrary/Models/Esfihas.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PizzariaLibrary.Models
 {
-    public class Pizzas
+    public class Esfihas
     {
         public int Id { get; set; }
         public int IdTipo { get; set; }
